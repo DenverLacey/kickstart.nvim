0f6cd2118646f2b3ad5d549daacbcb7eab788736
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2024-02-11 15:08:52 +1100

    Add colorschemes.

--- init.lua
+++ init.lua
@@ -27,7 +27,7 @@ Kickstart Guide:
 I have left several `:help X` comments throughout the init.lua
 You should run that command and read that help section for more information.
 
-In addition, I have some `NOTE:` items throughout the file.
+In addition, I have some `NOTE` items throughout the file.
 These are for you, the reader to help understand what is happening. Feel free to delete
 them once you know what you're doing, but they should serve as a guide for when you
 are first encountering a few different constructs in your nvim config.
@@ -200,20 +200,162 @@ require('lazy').setup({
     },
   },
 
+  -- [[ Color Themes ]]
+  -- See `rktjmp/lush.nvim` for creating colorschemes
+  --
+  -- Tokyonight
+  -- {
+  --   'folke/tokyonight.nvim',
+  --   priority = 1000,
+  --   lazy = false,
+  --   config = function()
+  --     require('tokyonight').setup {
+  --       style = 'storm',
+  --       styles = {
+  --         sidebars = "dark",
+  --         floats = "dark",
+  --       }
+  --     }
+  --     require('tokyonight').load()
+  --   end,
+  -- },
+  -- Melange
+  -- {
+  --   "savq/melange-nvim",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme('melange')
+  --   end,
+  -- },
+  -- Rose Pine 
+  -- {
+  --   "rose-pine/neovim",
+  --   name = "rose-pine",
+  --   priority = 1000,
+  --   config = function()
+  --     require("rose-pine").setup({
+  --       variant = "auto", -- auto, main, moon, or dawn
+  --       dark_variant = "moon", -- main, moon, or dawn
+  --       dim_inactive_windows = false,
+  --       extend_background_behind_borders = true,
+  --
+  --       enable = {
+  --         terminal = true,
+  --         legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+  --         migrations = true, -- Handle deprecated options automatically
+  --       },
+  --
+  --       styles = {
+  --         bold = true,
+  --         italic = true,
+  --         transparency = false,
+  --       },
+  --
+  --       groups = {
+  --         border = "muted",
+  --         link = "iris",
+  --         panel = "surface",
+  --
+  --         error = "love",
+  --         hint = "iris",
+  --         info = "foam",
+  --         note = "pine",
+  --         todo = "rose",
+  --         warn = "gold",
+  --
+  --         git_add = "foam",
+  --         git_change = "rose",
+  --         git_delete = "love",
+  --         git_dirty = "rose",
+  --         git_ignore = "muted",
+  --         git_merge = "iris",
+  --         git_rename = "pine",
+  --         git_stage = "iris",
+  --         git_text = "rose",
+  --         git_untracked = "subtle",
+  --
+  --         h1 = "iris",
+  --         h2 = "foam",
+  --         h3 = "rose",
+  --         h4 = "gold",
+  --         h5 = "pine",
+  --         h6 = "foam",
+  --       },
+  --
+  --       highlight_groups = {
+  --         -- Comment = { fg = "foam" },
+  --         -- VertSplit = { fg = "muted", bg = "muted" },
+  --       },
+  --
+  --       before_highlight = function(group, highlight, palette)
+  --         -- Disable all undercurls
+  --         -- if highlight.undercurl then
+  --         --     highlight.undercurl = false
+  --         -- end
+  --         --
+  --         -- Change palette colour
+  --         -- if highlight.fg == palette.pine then
+  --         --     highlight.fg = palette.foam
+  --         -- end
+  --       end,
+  --     })
+  --
+  --     vim.cmd("colorscheme rose-pine")
+  --     -- vim.cmd("colorscheme rose-pine-main")
+  --     -- vim.cmd("colorscheme rose-pine-moon")
+  --     -- vim.cmd("colorscheme rose-pine-dawn")
+  --   end,
+  -- },
+  -- Catppuccin
   {
-    -- Color Theme
-    'folke/tokyonight.nvim',
+    'catppuccin/nvim',
+    name = 'catppuccin',
     priority = 1000,
-    lazy = false,
     config = function()
-      require('tokyonight').setup {
-        style = 'storm',
-        styles = {
-          sidebars = "dark",
-          floats = "dark",
-        }
-      }
-      require('tokyonight').load()
+      require("catppuccin").setup({
+        flavour = "macchiato", -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+          light = "latte",
+          dark = "mocha",
+        },
+        transparent_background = false, -- disables setting the background color.
+        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+        term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+        dim_inactive = {
+          enabled = false, -- dims the background color of inactive window
+          shade = "dark",
+          percentage = 0.15, -- percentage of the shade to apply to the inactive window
+        },
+        no_italic = false, -- Force no italic
+        no_bold = false, -- Force no bold
+        no_underline = false, -- Force no underline
+        styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = { "italic" }, -- Change the style of comments
+          conditionals = { "italic" },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+        },
+        color_overrides = {},
+        custom_highlights = {},
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+        },
+      })
+
+      -- setup must be called before loading
+      vim.cmd.colorscheme "catppuccin"
     end,
   },
 
