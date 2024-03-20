46de89e9533f15cb8ee9e543b8232bd4c74b3eb4
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2024-03-21 02:59:10 +1100

    Remap Goto Type Def.

--- init.lua
+++ init.lua
@@ -714,7 +714,7 @@ local on_attach = function(_, bufnr)
   nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
   nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-  nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+  nmap('gO', require('telescope.builtin').lsp_type_definitions, '[G]oto Type Definition')
   nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
   nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
